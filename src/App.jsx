bf24f43315f7b7d797b0f0939af6372e6b2c5003
Abdullah <abdullahsufyan2007@gmail.com>
2023-11-05 08:51:06 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import UserContextProvider from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "https://mern-api-tau.vercel.app/";
+  axios.defaults.baseURL = "https://mern-api-tau.vercel.app";
   axios.defaults.withCredentials = true;
 
   return (
