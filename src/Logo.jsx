94512f4b6d870e0e9dc817063216619eb9d7a4e4
Abdullah <abdullahsufyan2007@gmail.com>
2023-11-06 00:02:28 -0800

    first commit

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -3,7 +3,7 @@ export default function Logo() {
   return (
       <div className="text-blue-600 font-bold flex gap-2 mb-4">
         <HiChatBubbleLeftRight className="text-2xl" />
-        Voice of Alquds
+        Chat app
       </div>
   );
 }
