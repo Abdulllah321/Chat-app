a4a488597fb673913a3d960faa21aa316fac410e
Abdullah <abdullahsufyan2007@gmail.com>
2024-05-20 23:46:52 +0500

    updated url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import UserContextProvider from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://abd-backend-chat.vercel.app";
   axios.defaults.withCredentials = true;
 
   return (
